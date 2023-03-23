98cc42cf91af54b3015d7416bc927e5ce8de81b5
esther <eniemed@g.educaand.es>
2023-03-23 19:37:17 +0100

    Realización de la documentación de la clase Persona y adición del README.md

--- src/main/kotlin/persona/Persona.kt
+++ src/main/kotlin/persona/Persona.kt
@@ -1,37 +1,59 @@
 package persona
-
+/**
+ * ## CLASE **PERSONA**
+ *
+ * Los atributos deben ser [nombre], [apellidos] y [dni]
+ *
+ * Los añadiremos de esa manera en el constructor
+ *
+ * Declararemos las variables debajo del mismo
+ *
+ * @author Esther Nieto Medina
+ **/
 class Persona(nombre_pers: String, apellidos_pers: String, dni_pers: String) {
-
+    /**
+     * Añadimos los setter y getter de [nombre], el cual se asegura de que no sobrepasa los **40 caracteres**
+     */
     var nombre = ""
         get() = field
         set(value) {
             if (value.length < 40) {
                 field = value
-            } else {
-                println("El nombre introducido es demasiado largo, inténtelo de nuevo")
             }
         }
+
+    /**
+     * Mediante setter y getter de [apellidos], nos aseguraremos de que no sobrepasa los 60 caracteres en total
+     */
     var apellidos = ""
         get() = field
         set(value) {
             if (value.length < 60) {
                 field = value
-            } else {
-                println("Los apellidos introducidos son demasiado largos, inténtelo de nuevo")
             }
         }
+
+    /**
+     * Mediante el getter y setter de [dni] nos aseguraremos de que el dni es exactamente 9 caracteres de largo
+     */
     var dni = ""
         get() = field
         set(value) {
             if (value.length == 9) {
                 field = value
-            } else {
-                println("El DNI introducido no es válido, inténtelo de nuevo")
             }
         }
 
-    fun mostrar() {
-        println("DNI: $dni" +
-                "Nombre: $apellidos, $nombre")
+    /**
+     * Por último, haremos una función [toString] que sirva como presentación para la persona
+     *
+     * El [toString] devolverá el [nombre], los [apellidos] y el [dni]
+     */
+    override fun toString(): String {
+        return "Persona(nombre='$nombre', apellidos='$apellidos', dni='$dni')"
     }
 }
+/**
+ * @since 23/03/2023
+ */
+
---
+++ src/main/kotlin/persona/README.md
@@ -0,0 +1,17 @@
+# PROYECTO DOCUMENTACIÓN
+
+*Proyecto realizado por Esther Nieto Medina, alumna de 1º DAW*
+
+## CLASE PERSONA
+
+En este proyecto implemento una clase *Persona*, que tiene los siguientes atributos:
+1. Nombre
+2. Apellidos
+3. DNI
+
+Esta clase persona tiene una función de presentarse, la cual está implementada mediante el siguiente **toString**:
+
+`override fun toString(): String {
+return "Persona(nombre='$nombre', apellidos='$apellidos', dni='$dni')"
+}`
+
